277973cd5a30e58bfc9176d1942085620c6de363
Cher <red_phoenix417@yahoo.com>
2019-07-11 23:32:41 +0000

    Added admin security authentication on /admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
+
   def show
   end
 end
